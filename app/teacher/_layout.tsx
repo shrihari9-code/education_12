ff5b5f030994a0c395b878633ddc56da578c4cdd
KirPot <kiran@dev.com>
2023-11-05 01:01:30 +0530

    configured teacher-routes layout file to have bottom-navigation tab

--- app/teacher/_layout.tsx
+++ app/teacher/_layout.tsx
@@ -1,8 +1,10 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 import React, { useState } from "react";
-import { Slot } from "expo-router";
+import { Link, Slot } from "expo-router";
 import Header from "../../components/Header";
 import Sidebar from "../../components/Sidebar";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import EntypoIcon from "react-native-vector-icons/Entypo";
 
 type Props = {};
 
@@ -17,8 +19,38 @@ const Layout = (props: Props) => {
       <Header toggleMenu={toggleMenu} />
       <Slot />
       {isMenuOpen && <Sidebar toggleSidebar={toggleMenu} />}
+
+      <View style={styles.bottomTab}>
+        <Link href="/teacher">
+          <EntypoIcon name="home" size={30} color="white" />
+        </Link>
+
+        <Pressable>
+          <Ionicons name="settings-sharp" size={30} color="white" />
+        </Pressable>
+
+        <Pressable>
+          <Ionicons name="settings-sharp" size={30} color="white" />
+        </Pressable>
+
+        <Link href="/settings">
+          <Ionicons name="settings-sharp" size={30} color="white" />
+        </Link>
+      </View>
     </>
   );
 };
 
 export default Layout;
+
+const styles = StyleSheet.create({
+  bottomTab: {
+    backgroundColor: "#145AAC",
+    position: "absolute",
+    bottom: 0,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 20,
+  },
+});
